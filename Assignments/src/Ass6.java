33eb16b715e8806cc6736d6d7d76415be54188e8
zoya070 <121374831+zoya070@users.noreply.github.com>
2023-02-02 17:56:53 +0530

    Update Ass6.java

--- Assignments/src/Ass6.java
+++ Assignments/src/Ass6.java
@@ -1,9 +1,46 @@
+import java.util.Scanner;
 
 public class Ass6 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+		 
+	    
+	    public static int calculateBill(int units)
+	    {
+	 
+	       
+	        if (units <= 100) {
+	            return units * 10;
+	        }
+	        else if (units <= 200) {
+	            return (100 * 10)
+	                + (units - 100)
+	                      * 15;
+	        }
+	        else if (units <= 300) {
+	            return (100 * 10)
+	                + (100 * 15)
+	                + (units - 200)
+	                      * 20;
+	        }
+	        else if (units > 300) {
+	            return (100 * 10)
+	                + (100 * 15)
+	                + (100 * 20)
+	                + (units - 300)
+	                      * 25;
+	        }
+	        return 0;
+	    }
+	 
+	    
+	    public static void main(String args[])
+	    {
+	    	Scanner sc=new Scanner(System.in);
+	    	System.out.println("Enter the Bill Unit");
+			int id=sc.nextInt();
+	 
+	        System.out.println(
+	            calculateBill(id));
+	    }
 	}
-
-}
