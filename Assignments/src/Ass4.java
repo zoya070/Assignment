1703366e3258ea4ed0a53e5276c5cfb354369723
zoya070 <121374831+zoya070@users.noreply.github.com>
2023-02-02 17:54:23 +0530

    Update Ass4.java

--- Assignments/src/Ass4.java
+++ Assignments/src/Ass4.java
@@ -3,7 +3,17 @@ public class Ass4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+Scanner sc=new Scanner(System.in);
+		System.out.println("enter size of the array");
+		int i1=sc.nextInt();
+	
+		List<Integer> arr=new ArrayList();
+		arr.add(sc.nextInt());
+		arr.add(sc.nextInt());
+		arr.add(sc.nextInt());
+		arr.add(sc.nextInt());
+		arr.forEach(s1->{if(s1%2!=0) 
+		System.out.println("this is all odd number  "+s1);});
 	}
 
 }
